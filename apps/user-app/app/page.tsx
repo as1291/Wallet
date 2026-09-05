9eabbce2ffa12c1b2359aafa36322dc3ae065b5b
as1291 <amrit.tiwari2002@gmail.com>
2026-09-05 23:16:40 +0530

    chore: test github with variables

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -18,6 +18,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "./lib/auth";
 import {AppBar}  from "@repo/ui/AppBar"
+//
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
